5b4ee63c4a0a4ddad205aa7fab5d7324fe781af4
shailesh097 <shailesh.pokharel.234@gmail.com>
2023-03-29 15:00:24 +0545

    added bunch more plugins

--- nvim/lua/shailesh/core/keymaps.lua
+++ nvim/lua/shailesh/core/keymaps.lua
@@ -12,4 +12,9 @@ keymap.set("n", "x", '"_x') --delete a character without copying to clipboard
 -- nvim-tree
 keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>")
 
-
+-- telescope
+keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>")
+keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>")
+keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>")
+keymap.set("n", "<leader>fb", "<cmd>Telescope buffers<cr>")
+keymap.set("n", "<leader>fh", "<cmd>Telescope help_tags<cr>")
--- nvim/lua/shailesh/plugins-setup.lua
+++ nvim/lua/shailesh/plugins-setup.lua
@@ -13,7 +13,7 @@ local packer_bootstrap = ensure_packer()
 vim.cmd([[
   augroup packer_user_config
     autocmd!
-    autocmd BufWritePost plugins-setup.lua source <afile> | PackerCompile
+    autocmd BufWritePost plugins-setup.lua source <afile> | PackerInstall
   augroup end
 ]])
 
@@ -23,19 +23,58 @@ if not status then
 end
 
 return packer.startup(function(use)
-
+    -- packer
 	use("wbthomason/packer.nvim")
+
+    -- colorscheme (nighfox)
 	use("EdenEast/nightfox.nvim")
+
+    -- comment with 
     use("numToStr/Comment.nvim")
     use("nvim-lua/plenary.nvim")
     use("nvim-tree/nvim-tree.lua")
 	use("kyazdani42/nvim-web-devicons")
 	use("nvim-lualine/lualine.nvim")
-	--fuzzyfinding
+	
+	-- fuzzyfinding
 	use({"nvim-telescope/telescope-fzf-native.nvim", run="make"})	
 	use({"nvim-telescope/telescope.nvim", branch="0.1.x"})
-    --hex color highlighter
+    
+	-- hex color highlighter
     use("norcalli/nvim-colorizer.lua")
+	
+	-- autocompletion
+	use("hrsh7th/nvim-cmp")
+	use("hrsh7th/cmp-buffer")
+	use("hrsh7th/cmp-path")
+
+	-- snippets
+	use("L3MON4D3/LuaSnip")
+	use("saadparwaiz1/cmp_luasnip")
+	use("rafamadriz/friendly-snippets")
+
+    -- managing and installing lsp servers, linters and formatters with mason
+    use("williamboman/mason.nvim")
+    use("williamboman/mason-lspconfig.nvim")
+    -- configuring the lsp servers
+    use("neovim/nvim-lspconfig")
+    use("hrsh7th/cmp-nvim-lsp")
+    use({
+        "glepnir/lspsaga.nvim",
+        branch = "main",
+        requires = {
+          { "nvim-tree/nvim-web-devicons" },
+          { "nvim-treesitter/nvim-treesitter" },
+        },
+    }) -- enhanced lsp utils
+    use("onsails/lspkind.nvim")
+
+    -- formatting and linting
+    use("jose-elias-alvarez/null-ls.nvim")
+    use("jayp0521/mason-null-ls.nvim")
+     
+
+
 
 	if packer_bootstrap then
 		requre("packer").sync()
