3f0e0b21995cf16ce5468830f6eb122d12171f1f
shailesh097 <77430755+shailesh097@users.noreply.github.com>
2025-03-14 07:10:48 -0700

    added options.lua

---
+++ nvim/options.lua
@@ -0,0 +1,43 @@
+require "nvchad.options"
+
+-- add yours here!
+
+local o = vim.o
+-- local g = vim.g 
+o.cursorlineopt ='both' -- to enable cursorline!
+o.mouse = 'a'
+o.clipboard = 'unnamedplus'
+o.swapfile = false
+o.completeopt = 'menuone,noinsert,noselect'  -- Autocomplete options
+
+-----------------------------------------------------------
+-- Neovim UI
+-----------------------------------------------------------
+o.number = true           -- Show line number
+o.showmatch = true        -- Highlight matching parenthesis
+o.foldmethod = 'marker'   -- Enable folding (default 'foldmarker')
+o.colorcolumn = '80'      -- Line lenght marker at 80 columns
+o.splitright = true       -- Vertical split to the right
+o.splitbelow = true       -- Horizontal split to the bottom
+o.ignorecase = true       -- Ignore case letters when search
+o.smartcase = true        -- Ignore lowercase for the whole pattern
+o.linebreak = true        -- Wrap on word boundary
+o.termguicolors = true    -- Enable 24-bit RGB colors
+o.laststatus=3            -- Set global statusline
+
+-----------------------------------------------------------
+-- Tabs, indent
+-----------------------------------------------------------
+o.expandtab = true        -- Use spaces instead of tabs
+o.shiftwidth = 4          -- Shift 4 spaces when tab
+o.tabstop = 4             -- 1 tab == 4 spaces
+o.smartindent = true      -- Autoindent new lines
+
+-----------------------------------------------------------
+-- Memory, CPU
+-----------------------------------------------------------
+o.hidden = true           -- Enable background buffers
+o.history = 100           -- Remember N lines in history
+o.lazyredraw = true       -- Faster scrolling
+o.synmaxcol = 240         -- Max column for syntax highlight
+o.updatetime = 250        -- ms to wait for trigger an event
